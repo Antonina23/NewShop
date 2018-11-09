82ec30ce658aa378de1ea33270872bdeccf46b2b
Antonina23 <tone4kawow@gmail.com>
2018-11-09 22:33:11 +0300

    Add length validation of name

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:shop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
